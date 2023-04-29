0597cd5ef638a01f0ca4d88acbc45351c5f0f9ce
Captain_Yi <125747843+tang7760@users.noreply.github.com>
2023-04-29 21:29:33 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -5,6 +5,7 @@ git clone --depth=1 --single-branch https://github.com/xiaoqingfengATGH/luci-the
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git
+git clone https://github.com/linkease/istore.git
 #git clone https://github.com/Lienol/openwrt-package.git
 
 #预置OpenClash内核和GEO数据
